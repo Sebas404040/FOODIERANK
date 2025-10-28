39fc2d6bfc0488e46767d17787d59606e05a5c91
Davisson-Adriel <droman819@unab.edu.co>
2025-10-28 11:54:21 -0500

    feat: implement database connection and retrieval functions

---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config'
+
+const uri = process.env.MONGO_URI;
+const db_name = process.env.DB_NAME;
+
+const cliente = new MongoClient(uri);
+let db;
+
+export async function conectarBD(){
+    try {
+        await cliente.connect();
+        console.log("DB conectada!!!");
+        db = cliente.db(db_name);
+    } catch (error) {
+        console.error("Error al conectar la BD:", error)
+    }
+}
+
+export function obtenerBD(){
+    if(!db) throw new Error("No se ha conectado la BD!!");
+    return db;
+}
