4b357b1cbd04d4433460e7063ee5600b8de4eeec
Davisson-Adriel <droman819@unab.edu.co>
2025-10-28 15:03:21 -0500

    feat: add controllers for managing dish categories with CRUD operations

---
+++ src/controllers/categorias_platos.controller.js
@@ -0,0 +1,47 @@
+import {
+    obtenerCategoriasPlatos,
+    crearCategoriaPlato,
+    eliminarCategoriaPlato,
+    actualizarCategoriaPlato
+} from "../services/categorias_platos.services.js";
+
+export async function getCategoriasPlatos(req, res) {
+    try {
+        const categorias = await obtenerCategoriasPlatos();
+        res.status(200).json(categorias);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+}
+
+export async function postCategoriaPlato(req, res) {
+    try {
+        const { nombre } = req.body;
+        const resultado = await crearCategoriaPlato(nombre);
+        res.status(201).json({ mensaje: "Categoría de plato creada exitosamente", resultado });
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+}
+
+export async function deleteCategoriaPlato(req, res) {
+    try {
+        const { id } = parseInt(req.params.id);
+        const resultado = await eliminarCategoriaPlato(id);
+        res.status(200).json({ mensaje: "Categoría de plato eliminada exitosamente", resultado });
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+}
+
+export async function patchCategoriaPlato(req, res) {
+    try {
+        const { id_categoriaActualizada } = parseInt(req.params.id);
+        const { id_plato } = req.body;
+        const resultado = await actualizarCategoriaPlato(id_categoriaActualizada, id_plato);
+        res.status(200).json({ mensaje: "Categoría de plato actualizada exitosamente", resultado });
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+}
+
