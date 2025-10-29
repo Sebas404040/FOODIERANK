4b0ab46f370b8e7ea19e9720c57be6481d8a3460
Davisson-Adriel <droman819@unab.edu.co>
2025-10-29 10:54:49 -0500

    feat: add functionality to like a restaurant review with validation

--- src/controllers/resenas_restaurantes.controller.js
+++ src/controllers/resenas_restaurantes.controller.js
@@ -2,7 +2,8 @@ import {
     obtenerResenasRestaurantes,
     agregarResenaRestaurante,
     eliminarResenaRestaurante,
-    obtenerResenasRestaurantePorId
+    obtenerResenasRestaurantePorId,
+    darLikeResenaRestaurante
 } from "../services/resenas_restaurantes.services.js";
 
 export async function getResenasRestaurantes(req, res) {
@@ -39,6 +40,17 @@ export async function getResenasRestaurantePorId(req, res) {
         const restauranteId = parseInt(req.params.id);
         const resenas = await obtenerResenasRestaurantePorId(restauranteId);
         res.status(200).json(resenas);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+}
+
+export async function putLikeResenaPlato(req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const id_usuario = req.body.id_usuario;
+        const resultado = await darLikeResenaRestaurante(id, id_usuario);
+        res.status(200).json({ mensaje: "Like agregado a la reseña de restaurante exitosamente", resultado });
     } catch (error) {
         res.status(500).json({ mensaje: error.message });
     }
