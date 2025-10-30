ea7afb49f9cef59fecce5e7d267ede5a13e50c09
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-30 01:09:19 -0500

    feat: :label: DTO added
    DTO authentication created succesfully
    
    none

--- src/DTOS/authenticationDTO.js
+++ src/DTOS/authenticationDTO.js
@@ -3,7 +3,9 @@ import { body } from "express-validator";
 export const registroDTO = [
     body("username").notEmpty().withMessage("El nombre es obligatorio"),
     body("password").isLength({min: 7}).withMessage("La contraseña debe ser mínimo de 6 caracteres"),
-    body("role").isString.withMessage("El rol debe ser una cadena de texto"),
+    body("role").isString().withMessage("El rol debe ser una cadena de texto"),
+    body("email").isEmail().withMessage("Correo no válido"),
+    body("telefono").isMobilePhone().withMessage("Número de teléfono no válido")
 ];
 
 export const inicioSesionDTO = [
